611e193b7e179a99dfc88401f8e4be814e778492
Nate Anderson <raynorue@gmail.com>
2019-12-18 08:38:51 -0600

    Updating preview mode a bit
    Cleaning up other code too

--- background.js
+++ background.js
@@ -4,9 +4,21 @@
 
 'use strict';
 
+var previewMode = false;
+
+/*
+first attempt at a global preview variable, this sends data to the content script.
+This is needed so as an "Extension" we can record one value to know what mode we're in, since the content_Script is cleared/runs on ever page refresh.
 
 chrome.webNavigation.onCompleted.addListener(function() {
     console.log('on ServiceNow!');
 
+    //send the initial preview mode value.
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {previewMode: previewMode}, function(response) {
+          console.log(response.farewell);
+        });
+      });
     
 }, {url: [{hostSuffix : 'service-now.com'}]});
+*/
--- content_injector.js
+++ content_injector.js
@@ -1,15 +1,55 @@
+
 (function () {
 
-    //createAndAppend('script', 'simpleMDE.js');
+    createAndAppend('script', 'imports/simpleMDE.js');
     //createAndAppend('style', 'SNarkdown.css');
     createAndAppend('script', 'imports/turndown.js');
-    createAndAppend('script', 'SNarkdown.js');
+    document.onreadystatechange = function () {
+        if (document.readyState === 'complete') {
+            createAndAppend('script', 'SNarkdown.js');
+        }
+      };
     
-    
-    function createAndAppend(elemName, fileName){
-    var s = document.createElement(elemName);
-    s.src = chrome.runtime.getURL(fileName);
-    (document.head || document.documentElement).appendChild(s);
+
+
+    function createAndAppend(elemName, fileName) {
+        var s = document.createElement(elemName);
+        s.src = chrome.runtime.getURL(fileName);
+        (document.head || document.documentElement).appendChild(s);
     }
 
 })();
+
+/*
+first attempt and enabling a global preview mode, issue here is that g_form is undefined... but otherwise did we wanted..
+the benefit of using g_form on the page, is that it will make the HTML fields "embedded" (rendering their HTML appropriately)
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(request.previewMode);
+
+        sendResponse({
+            farewell: "goodbye"
+        });
+        setTimeout(function () {
+            togglePreviewMode();
+        }, 300);
+
+        function togglePreviewMode() {
+            //previewMode = !previewMode;
+            var textAreas = document.getElementsByTagName('textarea');
+            for (let i = 0; i < textAreas.length; i++) {
+                let textArea = textAreas[i];
+                console.log('textArea: ', textArea);
+                if (textArea.id.indexOf('.simpleMDE') > -1) {
+                    textArea.style.display = request.previewMode ? 'none' : 'block';
+                    var snField = textArea.id.split('.')[1];
+                    var snElem = textArea.id.replace('.simpleMDE', '');
+                    g_form.setReadOnly(snField, request.previewMode);
+                    var tinyMCE = document.getElementById('element.' + snElem).getElementsByClassName('mce-tinymce mce-container mce-panel')[0];
+                    tinyMCE.style.display = request.previewMode ? 'none' : 'block';
+                    //hide this element and show and make readOnly it's original element
+                }
+            }
+        }
+    });
+    */
